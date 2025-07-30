482b8371117f61136ce42682bd27852675b83316
Dmitriev Vladislav <iapplepw@gmail.com>
2025-07-30 16:04:15 +0300

    Contents.swift
    minor update

--- Homework.playground/Pages/Lesson 1.xcplaygroundpage/Contents.swift
+++ Homework.playground/Pages/Lesson 1.xcplaygroundpage/Contents.swift
@@ -1,8 +1,8 @@
-// Вывести строку с целым количеством часов минут и секунд 
+// Вывести строку с целым количеством часов минут и секунд
 
 let seconds = 654321
-let hours:Int = (seconds/3600)
-let minutes:Int = ((seconds%3600)/60)
-let remainingSeconds:Int = (seconds%60)
+let hours = seconds/3600
+let minutes = (seconds%3600)/60
+let remainingSeconds = seconds%60
 
 print("\(seconds) секунд - это \(hours) часов, \(minutes) минут и \(remainingSeconds) оставшихся секунд")
